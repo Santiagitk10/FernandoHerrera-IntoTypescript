e2931b933d2942d4a9e89f253971db3985db7095
Santiago Sierra Herrera <santiago.sierra@sofka.com.co>
2023-05-16 10:21:05 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import './style.css'
+import viteLogo from '/vite.svg';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  hola Mundo
+`
+
+console.log('hola mundo');
+
+
+//Intrucciones para crear una aplicación con Vite con typescript
+// - Abrir una carpeta en la consola (hacerlo en la consola dev VSCode porque no me funcionó en bash)
+// - comando: npm create vite
+// - Dar nombre proyecto y Seleccionar vanilla y typescript
+// - ir a la carpeta del proyecto
+// - comando: npm install
+// - comando: npm run dev
+// - abrir en el navegador
